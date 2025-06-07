a10187e58c7075b34e44c991b664901c302567b9
Emilse Arias <emilsearias@gmail.com>
2025-06-07 11:51:13 -0300

    Add prismjs to style the code

--- src/app.js
+++ src/app.js
@@ -60,3 +60,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
